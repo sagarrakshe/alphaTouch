decac6daed0ceada60bb2b99314cb6a1c2475186
sagarrakshe <sagarrakshe2@gmail.com>
2013-06-19 19:21:26 +0530

    added separate file for 'pattern' and 'applications'

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ import commands
 import time
 import subprocess, shlex
 import os
+import ast
 
 #Global variables
 Left = 0
@@ -30,82 +31,27 @@ Top = 0
 Bottom = 0
 
 timeLimit = 0
-
 Dot=[]
+patterns = []
+applications = []
 
+def initialize():
+	global patterns
+	global applications
 
-'''
-dots = [(1300, 900), (2400, 900), (3500, 900), (4600, 900), (5700, 900),
-		(1300, 1825), (2400, 1825), (3500, 1825), (4600, 1825), (5700, 1825),
-		(1300, 2900), (2400, 2900), (3500, 2900), (4600, 2900), (5700, 2900),
-		(1300, 3975), (2400, 3975), (3500, 3975), (4600, 3975), (5700, 3975),
-		(1300, 5000), (2400, 5000), (3500, 5000), (4600, 5000), (5700, 5000)]
+	patternFile = open("mapPattern","r")
+	patternFileContent = patternFile.read()
+	patternFile.close()
 
-'''
-patterns = [
-			[[[2, 7, 12, 17, 22], [2, 3, 4], [4, 9, 14, 19, 24], [12, 13, 14]], 4, 'A'],
-			[[[3, 8, 13, 18, 23], [3, 4, 5, 10, 15, 14, 13], [13, 14, 15, 20, 25, 24, 23]], 3, 'B'],
-			[[[3, 2, 6, 11, 16, 22, 23]], 1, 'C'],
-			[[[3, 8, 13, 18, 23], [3, 4, 10, 15, 20, 24, 23]], 2, 'D'],
-			[[[3, 8, 13, 18, 23], [3, 4, 5], [13, 14], [23, 24, 25]], 4, 'E'],
-			[[[3, 8, 13, 18, 23], [3, 4, 5], [13, 14]], 3, 'F'], 
-			[[[4, 3, 2], [2, 7, 12, 17, 22], [22, 23, 24], [14, 19, 24], [13, 14, 15]], 5, 'G'],
-			[[[2, 7, 12, 17, 22], [4, 9, 14, 19, 24], [12, 13, 14]], 3, 'H'],
-			[[[2, 3, 4], [3, 8, 13, 18, 23], [22, 23, 24]], 3, 'I'],
-			[[[2, 3, 4], [3, 8, 13, 18, 23, 22, 17]], 2, 'J'],
-			[[[3, 8, 13, 18, 23], [5, 9, 13], [13, 19, 25]], 3, 'K'],
-			[[[3, 8, 13, 18, 23], [23, 24, 25]], 2, 'L'],
-			[[[1, 6, 11, 16, 21], [1, 7, 13], [5, 9, 13], [5, 10, 15, 20, 25]], 4, 'M'],
-			[[[1, 6, 11, 16, 21], [1, 7, 13, 19, 25], [5, 10, 15, 20, 25]], 4, 'N'],
-			[[[3, 2, 7, 12, 17, 22, 23, 24, 19, 14, 9, 4]], 1, 'O'],
-			[[[3, 8, 13, 18, 23], [3, 4, 5, 10, 15, 14, 13]], 2, 'P'],
-			[[[2, 7, 12, 17, 18, 19, 14, 9, 4, 3], [13, 19, 25]], 2, 'Q'],
-			[[[3, 8, 13, 18, 23], [3, 4, 5, 10, 15, 14, 13], [13, 19, 25]], 3, 'R'],
-			[[[4, 3, 2, 7, 12, 13, 14, 19, 24, 23, 22]], 1, 'S'],
-			[[[2, 3, 4], [3, 8, 13, 18, 23]], 2, 'T'],
-			[[[2, 7, 12, 17, 22], [22, 23, 24], [4, 9, 14, 19, 24]], 3, 'U'],
-			[[[1, 7, 12, 17, 23], [5, 9, 14, 18, 23]], 2, 'V'],
-			[[[1, 6, 11, 16, 21], [21, 17, 13], [13, 19, 25], [5, 10, 15, 20, 25]], 4, 'W'],
-			[[[1, 7, 13, 19, 25], [5, 9, 13, 17, 21]], 2, 'X'],
-			[[[4, 9, 14, 19, 24], [2, 7, 12], [12, 13, 14]], 3, 'Y'],
-			[[[1, 2, 3, 4, 5], [5, 9, 13, 17, 21], [21, 22, 23, 24, 25]], 3, 'Z'],
-			[[[3, 8, 13, 18, 23], [11, 12, 13, 14, 15]], 2, '+'],
-			[[[23], [17, 18, 19], [11, 12, 13, 14, 15]], 3, '^'],
-			[[[3, 2, 1, 6, 11, 16, 21, 22, 23, 24, 25, 20, 15, 10, 5, 4]], 1, '0']
-			]
+	patterns = eval(patternFileContent)
+	#print patterns
 
-applications ={
-				'A':'apt-get',
-				'B':'bash',
-				'C':'cd ~',
-				'D':'date',
-				'E':'echo',
-				'F':'find',
-				'G':'grep',
-				'H':'host',
-				'I':'ifconfig',
-				'J':'jobs',
-				'K':'kill',
-				'L':'ls',
-				'M':'man',
-				'N':'nmcli',
-				'O':'oclock',	
-				'P':'ping',
-				'Q':'qmake',
-				'R':'reboot',
-				'S':'sudo',
-				'T':'tty',
-				'U':'USB',
-				'V':'vim',
-				'W':'whoami',
-				'X':'xterm',
-				'Y':'yum',
-				'Z':'zsh',
-				
-				'+':'Calculator',
-				'^':'Wi-Fi',
-				'0':'reboot'
-				}
+	applicationFile = open("applications", "r")
+	applicationFileContent = applicationFile.read()
+	applicationFile.close()
+
+	applications = eval(applicationFileContent)
+	#print applications
 
 #Read from the configuration file.
 def createGrid():
@@ -139,6 +85,7 @@ def createGrid():
 			x += xDelta
 		y += yDelta
 		x = Left
+	
 	#print Dot
 
 def isPresent(point):
@@ -149,6 +96,7 @@ def isPresent(point):
 def pattern(points):
 	temp=[]
 	numbers=[]
+
 	for i in points:
 		for j in i:
 			point = isPresent(j)
@@ -160,24 +108,26 @@ def pattern(points):
 
 	for i in range(len(patterns)):
 		if numbers == patterns[i][0]:
-			
 			print applications[patterns[i][2]]
-
+	
 def main():
 	global timeLimit
+
+	initialize()
 
 	createGrid()
 
 	#print timeLimit
-	command = 'timeout ' + timeLimit +' synclient -m 100 > .pattern'
+	command = 'timeout ' + timeLimit +' synclient -m 100 > .patternDrawn'
 	subprocess.call(command, shell=True)
 
-	file = open('.pattern','r')
+	file = open('.patternDrawn','r')
 	lines = file.readlines()
 	
 	temp=[]
 	Lines=[]
 	lineNumber=-1
+
 	for i in lines:
 		line = filter(lambda x: x!='',i.split(' '))
 		if not (line[0]=='time'):
@@ -190,7 +140,7 @@ def main():
 	pattern(Lines)
 
 	file.close()
-	os.remove("./.pattern")
+	os.remove("./.patternDrawn")
 
 if __name__=='__main__':
 	main()
